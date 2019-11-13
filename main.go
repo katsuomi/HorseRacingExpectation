78467e5c34c0631a9649eb4f8f31a479bf055f1e
katsuomi <>
2019-11-13 15:45:13 +0900

    配列追加

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/PuerkitoBio/goquery"
@@ -13,40 +14,49 @@ import (
 )
 
 func main() {
-		id := "p201908050411" //ここにネットケイバの各レース毎のIDをぶち込む
-    url := "https://race.netkeiba.com/?pid=race&id="+id+"&mode=top"
+	id := "p201908050411" //ここにネットケイバの各レース毎のIDをぶち込む
+	url := "https://race.netkeiba.com/?pid=race&id=" + id + "&mode=top"
 
-    // Getリクエスト
-    res, _ := http.Get(url)
-    defer res.Body.Close()
+	// Getリクエスト
+	res, _ := http.Get(url)
+	defer res.Body.Close()
 
-    // 読み取り
-    buf, _ := ioutil.ReadAll(res.Body)
+	// 読み取り
+	buf, _ := ioutil.ReadAll(res.Body)
 
-    // 文字コード判定
-    det := chardet.NewTextDetector()
-    detRslt, _ := det.DetectBest(buf)
-    fmt.Println(detRslt.Charset)
-    // => EUC-JP
+	// 文字コード判定
+	det := chardet.NewTextDetector()
+	detRslt, _ := det.DetectBest(buf)
+	// => EUC-JP
 
-    // 文字コード変換
-    bReader := bytes.NewReader(buf)
-    reader, _ := charset.NewReaderLabel(detRslt.Charset, bReader)
+	// 文字コード変換
+	bReader := bytes.NewReader(buf)
+	reader, _ := charset.NewReaderLabel(detRslt.Charset, bReader)
 
-    // HTMLパース
-    doc, _ := goquery.NewDocumentFromReader(reader)
+	// HTMLパース
+	doc, _ := goquery.NewDocumentFromReader(reader)
 
-    // // titleを抜き出し
-    // rslt := doc.Find("body").("div").Text()
-    // fmt.Println(rslt)
+	var mapNameNumber map[string]int
+	mapNameNumber = map[string]int{}
+	var mapNameCount map[string]int
+	mapNameCount = map[string]int{}
+	var mapNumberCount map[int]int
+	mapNumberCount = map[int]int{}
 
-		doc.Find(".txt_l").Each(func(i int, s *goquery.Selection) {
-			// For each item found, get the band and title
-			href, _ := s.Find("a").Attr("href")
-			if (strings.Contains(href, "horse") == true) {
-				band := s.Find("a").Text()
-				fmt.Printf("Review %d: %s\n", i, band)
+	doc.Find(".txt_l").Each(func(i int, s *goquery.Selection) {
+		href, _ := s.Find("a").Attr("href")
+		if strings.Contains(href, "horse") == true {
+			band := s.Find("a").Text()
+			band2 := s.Parent().Find("td").Next().Next().First().Text()
+			if band != "" && band2 != "" {
+				band2Int, _ := strconv.Atoi(band2)
+				mapNameNumber[band] = band2Int
+				mapNameCount[band] = 0
+				mapNumberCount[band2Int] = 0
 			}
-		})
-
+		}
+	})
+	fmt.Printf("%d\n", mapNameNumber["ラッキーライラック"])
+	fmt.Printf("%d\n", mapNameCount)
+	fmt.Printf("%d\n", mapNumberCount)
 }
